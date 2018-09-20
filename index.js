7ac86ec01841ad86e0d20ba996951eed14feb013
harrietwharton <harrietwharton@gmail.com>
2018-09-20 12:55:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
 }
-function whisper(string) {
-  return string.toLowerCase()
-}
 
-function shout(string) {
-  console.log(hello)
-  return string.toUpperCase()
-}
+
+
 
 
 
